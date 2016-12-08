0c799412f4e8ef31c736deae34f9d3ea3f561ed6
a-mile <und3fnd@outlook.com>
2016-12-08 11:34:34 +0100

    Add validation

--- AspNet-RssReader-WebUI/ViewModels/AddSourceViewModel.cs
+++ AspNet-RssReader-WebUI/ViewModels/AddSourceViewModel.cs
@@ -1,8 +1,16 @@
-﻿namespace AspNet_RssReader_WebUI.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNet_RssReader_WebUI.ViewModels
 {
     public class AddSourceViewModel
     {
+        [Required]
+        [Display(Name = "Source name")]
         public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "RSS channel link")]
+        [Url(ErrorMessage = "Please enter a valid link")]
         public string Link { get; set; }
     }
 }
--- AspNet-RssReader-WebUI/ViewModels/ArticleContentViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AspNet_RssReader_WebUI.ViewModels
-{
-    public class ArticleContentViewModel
-    {
-        public string Title { get; set; }
-        public string Content { get; set; }
-        public string Link { get; set; }
-    }
-}
